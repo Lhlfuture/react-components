8af6e91b01dfc9f61260144ab2bffa0aeca95c2c
m18738156378 <m18738156378@163.com>
2020-04-09 00:30:35 +0800

    fe: antd

--- git-react/src/components/Composition.js
+++ git-react/src/components/Composition.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 
 //Dialog 定义组件外观和行为
@@ -27,23 +27,51 @@ import React, { Component } from 'react'
 
 //作用域插槽
 
-function Dialog (props){
-    const messages = {
-        foo: {title:'foo',content: 'foo!!!!!!!!'},
-        bar: {title:'bar',content: 'bar!!!!!!!!'}
-    }
-    console.log(messages[props.msg])
+// function Dialog (props){
+//     const messages = {
+//         foo: {title:'foo',content: 'foo!!!!!!!!'},
+//         bar: {title:'bar',content: 'bar!!!!!!!!'}
+//     }
+//     console.log(messages[props.msg])
 
-    const { def, footer } = props.children(messages[props.msg])
+//     const { def, footer } = props.children(messages[props.msg])
 
 
-return (
-    <div>
-        {def}
-        {footer}
-    </div>
-)
+// return (
+//     <div>
+//         {def}
+//         {footer}
+//     </div>
+// )
+// }
+
+
+// 如果 props.children 是jsx 它是只读的。
+
+// 实现 RedioGroup 和 Radio组件 通过RadioGroup 设置 Radio的name
+
+function RadioGroup(props) {
+    return (
+        <div>
+            {React.Children.map(props.children,radio=>{
+                //要修改虚拟dom 只能克隆它
+                // 参数1 是克隆对象
+                // 参数2 设置的是属性
+                return React.cloneElement(radio, {name: props.name})
+            })}
+        </div>
+    )
 }
+
+function Radio ({children, ...rest}) {
+    return (
+        <label>
+            <input type="radio"  {...rest}/>
+            {children}
+        </label>
+    )
+}
+
 
 
 
@@ -77,7 +105,7 @@ export default function Composition() {
 
     return (
         <div>
-            <Dialog msg="foo">
+            {/* <Dialog msg="foo">
                 {({title,content})=>({
                     def:(
                     <>
@@ -87,7 +115,15 @@ export default function Composition() {
                     ),
                     footer:<button onClick={()=>{alert('学习react')}}>确定</button>
                 })}
-            </Dialog>
+            </Dialog> */}
+
+            <RadioGroup name="mvvm">
+                <Radio value="vue">vue</Radio>
+                <Radio value="react">react</Radio>
+                <Radio value="node">node</Radio>
+            </RadioGroup>
+
+            {/* <div><input type="radio" name="mvvm">vue< /input></div> */}
         </div>
     )
 }
